7138ad84098735bddbb1247ee52cfe3c16053639
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-04-09 22:28:33 -0400

    added api fetch

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello from JS!")
+
+main();
+
+async function main() {
+    try {
+        const response = await fetch('https://api.ride42.ca/presentTrackdays');
+        if (!response.ok) throw new Error("Failed to get API Data")
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err.message)
+    }
+}
